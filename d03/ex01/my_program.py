b8f05301bdfa7b8c2d28f83d05a964d0644ee014
Valeria ZAMYATINA <vzamyati@e1r1p10.unit.ua>
2019-11-08 20:36:16 +0200

    done ex01, thankx Kolya

--- d03/ex01/my_program.py
+++ d03/ex01/my_program.py
@@ -1,2 +1,12 @@
+from local_lib.path import Path
+
 if __name__ == '__main__':
-	print("Hello")
+	folder = Path('./forex01')
+	folder.mkdir_p()
+	file = Path('./forex01/forex01.txt')
+	file.touch()
+	file.open()
+	file.write_text("For ex01 text")
+	print(file.text())
+
+	
--- d03/ex01/my_script.sh
+++ d03/ex01/my_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# to run the script correctly you have to do this commands first in the root of your project
+# python3 -m venv venv
+# source "./venv/bin/activate"
+
+
 pip3 --version
 pip3 install -t local_lib --upgrade git+https://github.com/jaraco/path.py.git > logs.log
 python3 my_program.py
